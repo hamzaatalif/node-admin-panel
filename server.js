a4cd60b904e7eba2c5aa0caf8aba1f933a85d82a
hamzaatalif <hamzaatalif@gmail.com>
2021-09-26 14:45:31 +0500

    login with env

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
+// host dependencies
+require("dotenv").config();
 const express = require("express");
+const path = require("path");
+// local dependencies
+const loginRouter = require("./routes/login");
+// initializations
 const app = express();
 const PORT = process.env.PORT || 5000;
+// middlewares
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static("./public"))
+app.use("/",loginRouter);
+
 
 
 const run = async () => {
