912279d07b1470c53543b41bf22deb880abc63da
hamzaatalif <hamzaatalif@gmail.com>
2021-09-29 15:46:39 +0500

    login with firebase

--- controllers/login.js
+++ controllers/login.js
@@ -2,12 +2,27 @@ require("dotenv").config();
 const path = require("path");
 const jwt = require("jsonwebtoken");
 
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+
+const firebaseApp = require("../db/connect");
+
+const db = getFirestore();
+
 const getLogin = (req, res) => {
-    return res.sendFile(path.join(__dirname, "../public/login.html"));
+    return res.sendFile(path.join(__dirname,"../public/login.html"))
 }
 
-const postLogin = (req, res) => {
-    return res.redirect("/")
+const postLogin = async (req, res) => {
+    const userInput = req.body.adminPass;
+    const querySnapshot = await getDocs(collection(db, "admins"));
+    const adminPass = querySnapshot.docs.map((doc) => {
+        return doc.data().pass;
+    });
+    adminPass.toString();
+    if (userInput == adminPass) {
+        return res.redirect("/")
+    }
+    return res.redirect("/login")
 }
 
 module.exports = {
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 // host dependencies
 require("dotenv").config();
 const express = require("express");
-import { initializeApp } from "firebase/app";
 // local dependencies
 const loginRouter = require("./routes/login");
 const dashboardRouter = require("./routes/dashboard");
@@ -13,30 +12,10 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.static("./public"))
 app.use("/", dashboardRouter);
 app.use("/login", loginRouter);
-// config
-const {
-    apiKey,
-    authDomain,
-    projectId,
-    storageBucket,
-    messagingSenderId,
-    appId,
-    measurementId
-} = process.env;
 
-const firebaseConfig = {
-    apiKey: apiKey,
-    authDomain: authDomain,
-    projectId: projectId,
-    storageBucket: storageBucket,
-    messagingSenderId: messagingSenderId,
-    appId: appId,
-    measurementId: measurementId
-}
 
 const run = async () => {
     try {
-        const firebaseApp = await initializeApp(firebaseConfig);
         app.listen(PORT, console.log(`Server is listening on port: ${PORT}...`))
     } catch (error) {
         console.error(error)
