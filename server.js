4bc0c7da34764dcb4c7306c6626141330976af14
hamzaatalif <hamzaatalif@gmail.com>
2021-09-29 14:16:06 +0500

    connected firebase

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // host dependencies
 require("dotenv").config();
 const express = require("express");
-const path = require("path");
+import { initializeApp } from "firebase/app";
 // local dependencies
 const loginRouter = require("./routes/login");
 const dashboardRouter = require("./routes/dashboard");
@@ -13,11 +13,30 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.static("./public"))
 app.use("/", dashboardRouter);
 app.use("/login", loginRouter);
+// config
+const {
+    apiKey,
+    authDomain,
+    projectId,
+    storageBucket,
+    messagingSenderId,
+    appId,
+    measurementId
+} = process.env;
 
-
+const firebaseConfig = {
+    apiKey: apiKey,
+    authDomain: authDomain,
+    projectId: projectId,
+    storageBucket: storageBucket,
+    messagingSenderId: messagingSenderId,
+    appId: appId,
+    measurementId: measurementId
+}
 
 const run = async () => {
     try {
+        const firebaseApp = await initializeApp(firebaseConfig);
         app.listen(PORT, console.log(`Server is listening on port: ${PORT}...`))
     } catch (error) {
         console.error(error)
