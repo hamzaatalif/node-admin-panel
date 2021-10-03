cf7c6656c87b84658c428ab4a6a3354594cd9fd5
hamzaatalif <hamzaatalif@gmail.com>
2021-10-04 01:52:04 +0500

    firebase cleaned

--- controllers/login.js
+++ controllers/login.js
@@ -2,44 +2,16 @@ require("dotenv").config();
 const path = require("path");
 const jwt = require("jsonwebtoken");
 
-import { getFirestore, collection, getDocs } from "firebase/firestore";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
-const firebaseApp = require("../db/connect");
 
-const db = getFirestore();
 
 const getLogin = (req, res) => {
     return res.sendFile(path.join(__dirname, "../public/login.html"))
 }
 
-const auth = getAuth();
 
 const postLogin = async (req, res) => {
 
-    signInWithEmailAndPassword(auth, req.body.adminEmail, req.body.adminPass)
-        .then((userCredential) => {
-            // Signed in 
-            return res.redirect("/")
-            const user = userCredential.user;
-            // ...
-        })
-        .catch((error) => {
-            return res.redirect("/login")
-            const errorCode = error.code;
-            const errorMessage = error.message;
-        });
-
-    // const userInput = req.body.adminPass;
-    // const querySnapshot = await getDocs(collection(db, "admins"));
-    // const adminPass = querySnapshot.docs.map((doc) => {
-    //     return doc.data().pass;
-    // });
-    // adminPass.toString();
-    // if (userInput == adminPass) {
-    //     return res.redirect("/")
-    // }
-    // return res.redirect("/login")
 }
 
 module.exports = {
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require("express");
 // local dependencies
 const loginRouter = require("./routes/login");
 const dashboardRouter = require("./routes/dashboard");
+const authUser = require("./middleware/auth");
 // initializations
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -12,6 +13,8 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.static("./public"))
 app.use("/", dashboardRouter);
 app.use("/login", loginRouter);
+app.use("/",authUser);
+
 
 
 const run = async () => {
